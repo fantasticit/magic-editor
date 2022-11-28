db36845fcba8660f1926365c0e85aa86c9326e5e
fantasticit <fantasticit@163.com>
2022-11-28 10:51:10 +0800

    fix insertPos

--- src/extensions/iframe/iframe.ts
+++ src/extensions/iframe/iframe.ts
@@ -1,6 +1,9 @@
 import { mergeAttributes, Node, nodeInputRule } from "@tiptap/core";
 import { ReactNodeViewRenderer } from "@tiptap/react";
 
+import { NodeSelection } from "../../prosemirror";
+import { isNodeEmpty } from "../../utilities";
+
 import { IframeView } from "./iframe-view";
 
 export interface IIframeAttrs {
@@ -74,42 +77,26 @@ export const Iframe = Node.create({
         const attrs = options || { url: "" };
 
         const { selection } = state;
-        const { empty } = selection;
-        const head = selection.$head;
+        const currentNode = selection.$head.node(selection.$head.depth);
+        const isEmpty = currentNode ? isNodeEmpty(currentNode) : true;
+        const insertPos = !isEmpty
+          ? state.tr.selection.from + 1
+          : state.tr.selection.from - 1;
 
-        if (empty) {
-          return chain()
-            .insertContent({
-              type: this.name,
-              attrs
-            })
-            .run();
-        } else {
-          const node = head.node(head.depth);
+        return chain()
+          .command(({ tr, dispatch, state }) => {
+            if (dispatch) {
+              tr.insert(
+                insertPos,
+                state.schema.nodes[this.name].create(attrs)
+              ).setSelection(NodeSelection.create(tr.doc, insertPos));
 
-          if (node.type.name === state.schema.topNodeType.name) {
-            return chain()
-              .command(({ tr, dispatch, state }) => {
-                if (dispatch) {
-                  tr.replaceSelectionWith(
-                    state.schema.nodes[this.name].create(attrs)
-                  ).scrollIntoView();
-                }
+              dispatch(tr);
+            }
 
-                return true;
-              })
-              .run();
-          }
-
-          return chain()
-            .insertContentAt(head.after(), [
-              {
-                type: this.name,
-                attrs
-              }
-            ])
-            .run();
-        }
+            return true;
+          })
+          .run();
       }
     };
   },
